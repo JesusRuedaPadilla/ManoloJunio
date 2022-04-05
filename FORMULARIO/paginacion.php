cfefd09cb42475dbcb8e0f4a8e76abea46d93d87
Jesus Rueda Padilla <jruepad413@g.educaand.es>
2022-04-05 13:00:58 +0200

    Editar Paginacion paso 1

--- FORMULARIO/paginacion.php
+++ FORMULARIO/paginacion.php
@@ -14,10 +14,11 @@ $c=$p-1;
 
 for ($i=0;$i<count($lista);$i++)
 {
-    echo "Correo : " .$lista[$i]['correo']. " Contraseña : " .$lista[$i]['contrasena']. "<br>";
- 
+    echo "Correo : " .$lista[$i]['correo']. " Contraseña : " .$lista[$i]['contrasena']. "<a href='paginacion.php?g=editar'>Editar</a>" ."<br>";
+    
    
 }
+
 
 if($p==1){
 
@@ -44,3 +45,15 @@ else if($p==ConexionBD::NumPaginas(2)){
 
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
